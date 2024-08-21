c7541f633ef136cde3265271f72374e79d4fc4b6
Nikhil Sharma <nikhilzs@amazon.com>
2024-08-21 12:22:46 +0530

    Initial Commit

---
+++ src/test/java/com/amazon/drool/DroolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazon.drool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
